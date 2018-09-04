558f6dd7feebf55406883a51dcc86a2202d86b1b
guoding <guoding@ct108.com>
2018-09-04 15:01:25 +0800

    .

--- generator/index.js
+++ generator/index.js
@@ -1,3 +1,11 @@
 module.exports = (api, options) => {
-  console.log(api,options);
+  api.extendPackage({
+    scripts: {
+      test: 'vue-cli-service test'
+    }
+  })
+
+  // 复制并用 ejs 渲染 `./template` 内所有的文件
+  api.render('./template');
+  console.log(options);
 }
